935ab1976330bd2d3339253334958434311d9f9d
CarlosMoreno3 <101369176+CarlosMoreno3@users.noreply.github.com>
2023-04-20 21:56:44 -0600

    Actualizacion Clase Camino

--- Proyecto_Tower_Defense/src/proyecto_tower_defense/Camino.java
+++ Proyecto_Tower_Defense/src/proyecto_tower_defense/Camino.java
@@ -9,26 +9,62 @@ package proyecto_tower_defense;
  * @author cadam
  */
 public class Camino {
-    private Tropa[] tropas; //Array que almacena las tropas en el camino
+
+    private Nodo inicio; // inicio de la lista
     private int longitud; //Logitud del camino
-    
-    public Camino(int longitud){
-        this.longitud =  longitud;
-        tropas = new Tropa[longitud];
+
+    public class Nodo {
+
+        Tropa tropa; //tropa que se almacena en el nodo
+        Nodo siguiente;
+
+        public Nodo(Tropa tropa) {
+            this.tropa = tropa;
+            this.siguiente = null;
+        }
     }
-    
+
+    public Camino(int longitud) {
+        this.longitud = longitud;
+        inicio = null;
+    }
+
     //agregar tropa en posicion determinada del camino
-    public void agregarTropa(Tropa tropa, int posicion){
-        tropas[posicion]= tropa;
+    public void agregarTropa(Tropa tropa, int posicion) {
+        if (posicion < 0 || posicion >= longitud) {
+            throw new IllegalArgumentException("Posición inválida");
+        }
+        Nodo nuevoNodo = new Nodo(tropa);
+        if (posicion == 0) {
+            nuevoNodo.siguiente = inicio;
+            inicio = nuevoNodo;
+        } else {
+            Nodo nodoAnterior = obtenerNodo(posicion - 1);
+            nuevoNodo.siguiente = nodoAnterior.siguiente;
+            nodoAnterior.siguiente = nuevoNodo;
+        }
     }
     
+    // Obtener el nodo en posición determinada del camino
+    private Nodo obtenerNodo(int posicion) {
+        Nodo nodo = inicio;
+        for (int i = 0; i < posicion; i++) {
+            nodo = nodo.siguiente;
+        }
+        return nodo;
+    }
+
     //obtener tropa en posicion determinada del camino
-    public Tropa obtenerTropa(int posicion){
-        return tropas[posicion];
+    public Tropa obtenerTropa(int posicion) {
+        if(posicion < 0 || posicion >= longitud){
+            throw new IllegalArgumentException("Posición inválida");
+        }
+        Nodo nodo = obtenerNodo(posicion);
+        return nodo.tropa;
     }
-    
+
     //obtener la longitud del camino
-    public int obtenerLongitud(){
+    public int obtenerLongitud() {
         return longitud;
     }
 }
